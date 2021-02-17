2b6eee8e4e2753a6447d177c2cb834362f61cc58
koki014 <koki.suleymanov@gmail.com>
2021-02-17 17:39:56 +0400

    cors

--- tech/settings.py
+++ tech/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'main',
     'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
