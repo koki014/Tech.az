92564b057f2efec754482972cabbbc46653260cc
Nizami <koki.suleymanov@gmail.com>
2021-05-27 19:36:51 +0400

    banner

--- tech/urls.py
+++ tech/urls.py
@@ -49,7 +49,7 @@ urlpatterns = [
 
     path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
-    path('admin', admin.site.urls),
+    path('', admin.site.urls),
     path('api-doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('account.urls', namespace='account')),
