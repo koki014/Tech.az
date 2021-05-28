0442d4688e564d9a9776a2d2ab8eb9e143f6d1b5
Elvin <husubayli@gmail.com>
2021-05-28 18:13:23 +0400

    fixeed user endpoint

--- account/api/urls.py
+++ account/api/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
     path('login/', LoginAPI.as_view(), name='login'),
     path('password-reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path('user-profile/', ProfileAPIView.as_view(), name='user_profile'),
-    path('user-profile/<int:user_id>/articles/', UserReadOnlyModelViewSets.as_view({'get': 'get_users_news'}), name='get_news'),
+    path('profile/', ProfileAPIView.as_view(), name='user_profile'),
+    path('profile/<int:user_id>/articles/', UserReadOnlyModelViewSets.as_view({'get': 'get_users_news'}), name='get_news'),
 
 ]
 
--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 app_name = 'account'
 
 urlpatterns = [
-    path('', include('account.api.urls'))
+    path('user/', include('account.api.urls'))
 ]
